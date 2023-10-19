94d507f1b5293eb3e0ac19aa6f765adc02041485
aryan raj <aryan.raj@airtel.com>
2023-10-19 08:33:59 +0530

    custom fetch added

---
+++ customFetch/customFetch.js
@@ -0,0 +1,49 @@
+function customFetch(url, options) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(options.method || 'GET', url);
+
+    if (options.headers) {
+      for (let header in options.headers) {
+        xhr.setRequestHeader(header, options.headers[header]);
+      }
+    }
+
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve({
+          status: xhr.status,
+          statusText: xhr.statusText,
+          text: () => Promise.resolve(xhr.responseText),
+          json: () => Promise.resolve(JSON.parse(xhr.responseText)),
+        });
+      } else {
+        reject({
+          status: xhr.status,
+          statusText: xhr.statusText,
+        });
+      }
+    };
+
+    xhr.onerror = function () {
+      reject({
+        status: xhr.status,
+        statusText: xhr.statusText,
+      });
+    };
+
+    xhr.send(options.body);
+  });
+}
+
+// Example usage:
+customFetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
