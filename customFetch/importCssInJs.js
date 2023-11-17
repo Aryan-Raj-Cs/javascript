d86d02c3cff5b98d833b8fe4c841073939a5d0bb
aryan raj <aryan.raj@airtel.com>
2023-11-17 14:57:03 +0530

    import css work in vanilla js

--- customFetch/importCssInJs.js
+++ customFetch/importCssInJs.js
@@ -1,7 +1,21 @@
 import styles from "./style.css" assert { type: "css" };
 document.adoptedStyleSheets = [styles];
+document.addEventListener('DOMContentLoaded', function () {
+    const h1Element = document.getElementById("myh2");
+    console.log("element",h1Element,styles)
+    h1Element.classList.add('heading');
+  });
+
+// in module bundler these type syntax is taken care by webpack by css-loader and style-loader
+// import  "./style.css" 
 // document.addEventListener('DOMContentLoaded', function () {
 //     const h1Element = document.getElementById("myh2");
 //     console.log("element",h1Element,styles)
 //     h1Element.classList.add('heading');
 //   });
+
+  (function hello(){
+    console.log("hello")
+  })()
+
+
